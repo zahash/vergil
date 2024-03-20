99c6a61d4b35d84a03e4f17e150e3368c41239ac
zahash <zahash.z@gmail.com>
2024-03-20 20:30:33 +0530

    hook the custom parser into the clap machinery

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,49 @@
 use clap::Parser;
-
 use num_bigint::BigUint;
 use num_traits::Num;
 
 #[derive(Parser, Debug)]
 enum Cli {
-    Hex { input: String },
-    Bin { input: String },
-    Dec { input: String },
+    Hex {
+        #[arg(value_parser = num_parser)]
+        input: BigUint,
+    },
+    Bin {
+        #[arg(value_parser = num_parser)]
+        input: BigUint,
+    },
+    Dec {
+        #[arg(value_parser = num_parser)]
+        input: BigUint,
+    },
 }
 
-#[derive(Debug)]
+fn num_parser(s: &str) -> Result<BigUint, CliError> {
+    match s.get(0..2) {
+        Some("0x") => {
+            <BigUint as Num>::from_str_radix(&s[2..], 16).map_err(|_| CliError::InvalidNum)
+        }
+        Some("0b") => {
+            <BigUint as Num>::from_str_radix(&s[2..], 2).map_err(|_| CliError::InvalidNum)
+        }
+        _ => <BigUint as Num>::from_str_radix(&s, 10).map_err(|_| CliError::InvalidNum),
+    }
+}
+
+#[derive(thiserror::Error, Debug)]
 enum CliError {
+    #[error("invalid number")]
     InvalidNum,
-}
-
-fn invalid_num<E>(_: E) -> CliError {
-    CliError::InvalidNum
 }
 
 impl Cli {
     fn run(self) -> Result<(), CliError> {
         match self {
-            Cli::Hex { input } => println!("{:X}", parse(&input)?),
-            Cli::Bin { input } => println!("{:b}", parse(&input)?),
-            Cli::Dec { input } => println!("{}", parse(&input)?),
+            Cli::Hex { input } => println!("{:X}", input),
+            Cli::Bin { input } => println!("{:b}", input),
+            Cli::Dec { input } => println!("{}", input),
         }
         Ok(())
-    }
-}
-
-fn parse(s: &str) -> Result<BigUint, CliError> {
-    match s.get(0..2) {
-        Some("0x") => <BigUint as Num>::from_str_radix(&s[2..], 16).map_err(invalid_num),
-        Some("0b") => <BigUint as Num>::from_str_radix(&s[2..], 2).map_err(invalid_num),
-        _ => <BigUint as Num>::from_str_radix(&s, 10).map_err(invalid_num),
     }
 }
 
